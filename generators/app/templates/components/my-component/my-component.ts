51aee7fdd3f7bba0c00056a0a0748881199326bb
Adrian Guillen <aguillen@uxland.es>
2020-11-26 10:36:57 +0100

    fix: add ts-ignore in my-component.ts

--- generators/app/templates/components/my-component/my-component.ts
+++ generators/app/templates/components/my-component/my-component.ts
@@ -1,15 +1,18 @@
+//@ts-ignore
+import styles from './styles.scss';
 import { html, LitElement, property, customElement, query, css, unsafeCSS} from 'lit-element';
 import {locale} from "@uxland/uxl-prism";
 import {template} from './template';
-import styles from './styles.scss';
+
 
 @customElement('<%= moduleId %>')
 export class <%= camelCase %> extends (locale(LitElement)) {
 
+
+
   render() {
     return html`${template(this)}`;
   }
-
 
   static get styles() {
     return css`${unsafeCSS(styles)}`;
